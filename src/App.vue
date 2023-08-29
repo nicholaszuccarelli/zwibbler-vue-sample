939b9c6ac23b70527de6a7722512071d99f5733e
nicholaszuccarelli <nicholaszuccarelli@gmail.com>
2023-08-29 12:23:05 +1000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="width: 100vh; height: 100vh; display:  flex; flex-direction: row;">
+    <div v-if="!!zctx" style="flex: 0 0 300px; background-color: lightgray;">
+      <div>
+        <b>Sidebar Elements</b>
+      </div>
+      <button
+          :disabled="zctx.getCurrentTool() === 'rectangle'"
+          @click="zctx.useRectangleTool()"
+      >
+        Rectangle
+      </button>
+      <button
+          :disabled="zctx.getCurrentTool() === 'brush'"
+          @click="zctx.useBrushTool()"
+      >
+        Brush
+      </button>
+      <button
+          :disabled="zctx.getCurrentTool() === 'text'"
+          @click="zctx.useTextTool()"
+      >
+        Font
+      </button>
+    </div>
+    <div style="flex: 1 0 auto; position: relative;">
+      <zwibbler ref="zwibblerContainer" z-no-auto-init class="full flex-col">
+        <div z-canvas class="stretch"></div>
+      </zwibbler>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// uncomment when testing markRaw
+// import { markRaw } from 'vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      leftDrawerOpen: false,
+      zctx: null
+    }
+  },
+  methods: {
+    toggleLeftDrawer () {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    }
+  },
+  mounted () {
+    const scope = window.Zwibbler.attach(this.$refs.zwibblerContainer, {})
+    this.zctx = scope.ctx
+    // uncomment when testing markRaw (and comment above)
+    // this.zctx = markRaw(scope.ctx)
+  },
+  beforeUnmount () {
+    if (this.zctx) {
+      console.log('DESTROY ZWIBBLER')
+      this.zctx.destroy()
+      this.zctx = null
+    }
   }
 }
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
